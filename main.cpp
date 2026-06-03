5dbd52be4c48d7c932da600f1b45ff15452fabb6
Artem <a.tra4017@gmail.com>
2026-06-03 13:27:20 +0300

    Initial commit (Lab 3 project from local folder)

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "========================================" << std::endl;
+    std::cout << "GitLab CI/CD Compilation Test" << std::endl;
+    std::cout << "Laboratory Work #3" << std::endl;
+    std::cout << "Status: Success" << std::endl;
+    std::cout << "========================================" << std::endl;
+    
+    return 0;
+}
